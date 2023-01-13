0f0df4e5a9deee3b466698574055bc251db23f6a
inti7ary <j129q18@gmail.com>
2023-01-13 21:23:08 +0300

    Go through all pages

--- sel.py
+++ sel.py
@@ -12,10 +12,23 @@ link = input("link: ")
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(link)
 driver.maximize_window()
-time.sleep(1)
+wait = WebDriverWait(driver, timeout=10)
 
 
 # Logging in 
 driver.find_element('name', 'login').send_keys(login)
 driver.find_element('name', 'password').send_keys(password)
 driver.find_element('xpath', "//*[@type='submit']").click()
+
+
+def table_loaded(driver: webdriver.Chrome):
+    return driver.find_elements('class name', 'pagination')
+
+
+while True:
+    wait.until(table_loaded)
+    next_button = driver.find_element('css selector', '.pagination > li:last-child > a')
+    if next_button.get_attribute('href') == driver.current_url:
+        break
+    else:
+        next_button.click()
