4f9b27b12e83d38afc393a9fbacefaa86bd06c57
m1zum0no <m3nh3r4chan@gmail.com>
2023-01-20 13:07:00 +0300

    changed variable name

--- automate_mailing.py
+++ automate_mailing.py
@@ -16,11 +16,11 @@ payload = {
 }
 
 
-with requests.Session() as session, open('test.py', 'r') as users:
-    for user in users:
-        user = list(user.split(':'))
-        payload['to'] = user[0]
-        payload['text'] = construct_message(*user[1:])    
+with requests.Session() as session, open('names.py', 'r') as parsed_data:
+    for client_record in parsed_data:
+        client_record = list(client_record.split(':'))
+        payload['to'] = client_record[0]
+        payload['text'] = construct_message(*client_record[1:])    
         response = session.post(URL, json=payload)
         logger.info(f'SMS sent to {payload["to"]}, response status: {response.status_code}')
 
